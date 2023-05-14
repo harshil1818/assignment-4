902a8955543f72cdfd6444862c6e8ed53e0a067f
harshil1818 <129713252+harshil1818@users.noreply.github.com>
2023-05-15 01:03:04 +0530

    Add files via upload

---
+++ Q-3.py
@@ -0,0 +1,12 @@
+
+import random as r
+for i in range(11):
+    a=r.randint(1,10)
+b=r.randint(1,10)
+answer=a*b
+print("Question",i+1,a,"x",b)
+answer=int(input(""))
+if answeruser==answer:
+    print("Answer is correct")
+else:
+    print("wrong\",Answer is",a*b)
